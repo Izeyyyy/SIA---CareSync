3a2e33f88e299aa6c7fe6dc92589021329f855ab
LAB22_USER ON WS08 <L22X16W08@LabHCI.net>
2026-07-08 17:48:30 +0800

    added dto to authentication feature

---
+++ backend/src/main/java/edu/cit/gaane/caresync/features/authentication/dto/LoginResponse.java
@@ -0,0 +1,53 @@
+package edu.cit.gaane.caresync.features.authentication.dto;
+
+public class LoginResponse {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private char middleInitial;
+    private String email;
+    private String role;
+
+    public LoginResponse() {}
+
+    public LoginResponse(
+            Long id,
+            String firstName,
+            String lastName,
+            char middleInitial,
+            String email,
+            String role
+    ) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.middleInitial = middleInitial;
+        this.email = email;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public char getMiddleInitial() {
+        return middleInitial;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
