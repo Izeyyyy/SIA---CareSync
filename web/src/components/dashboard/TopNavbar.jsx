802271ded1dd6fdfcea071d9616cac1a04cd2ef0
unknown <hammermikuzen@gmail.com>
2026-07-12 20:33:30 +0800

    beautified admin page

--- web/src/components/dashboard/TopNavbar.jsx
+++ web/src/components/dashboard/TopNavbar.jsx
@@ -1,22 +1,60 @@
+import { useAuth } from "../../context/AuthContext";
+import { Bell } from "lucide-react";
+
 export default function TopNavbar() {
 
-    const user = JSON.parse(
-        localStorage.getItem("user")
-    );
-
+    const { user } = useAuth();
 
     return (
+
         <header className="dashboard-navbar">
 
-            <h3>
-                Admin Workspace
-            </h3>
+            <div className="navbar-left">
 
+                <h2>
 
-            <span>
-                {user?.firstName || "Administrator"}
-            </span>
+                    Welcome back,
+
+                    {" "}
+
+                    {user?.firstName || "User"}
+
+                </h2>
+
+            </div>
+
+            <div className="navbar-right">
+
+                <button className="notification-btn">
+
+                    <Bell size={20}/>
+
+                </button>
+
+                <div className="navbar-profile">
+
+                    <div>
+
+                        <div className="profile-name">
+
+                            {user?.firstName} {user?.lastName}
+
+                        </div>
+
+                        <div className="profile-role">
+
+                            {user?.role}
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
 
         </header>
+
     );
+
 }
